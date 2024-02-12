1a4f0d19477ca852372a3685716cab942f7a129c
zabolotnipro <159847561+zabolotnipro@users.noreply.github.com>
2024-02-13 11:20:05 +1300

    Add files via upload

---
+++ Work1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Work1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Este comando es para saludarle al mundo, tu kp mas querido
+        print("Mbateko mundo mi kp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
